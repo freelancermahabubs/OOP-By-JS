51436f067a767044352b99c4b9c085fd7fe16c8f
freelancermahabubs <info.freelancermahabub@gmail.com>
2024-08-15 09:45:08 +0600

    added done

---
+++ index.js
@@ -0,0 +1,8 @@
+let person  =  {
+    firstName : "Mahabub",
+    lastName : "Alom",
+    age: 22,
+    getName : () => {
+        console.log(`My Name is ${person.firstName} ${person.lastName}`)
+    }
+}
